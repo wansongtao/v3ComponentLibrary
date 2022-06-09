0990e7a98afe1527f26ae267a27e6a1658401d81
wansongtao <18101837209@163.com>
2022-06-09 14:30:20 +0800

    build:设置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,11 @@ export default defineConfig({
   ],
   server: {
     open: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+    extensions: ['.js', '.json', '.ts'] // 使用路径别名想省略的后缀名
   }
 });
