76e9fb1fe07616ad69458b3bc08d86d5056ed2a1
wansongtao <18101837209@163.com>
2022-06-10 10:05:00 +0800

    perf:全局提示组件message封装优化，添加返回值和详细注释

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -4,9 +4,23 @@ import 'ant-design-vue/es/message/style/css';
 /**
  * @description 全局提示，ant-design的message组件
  * @param config 
+ * @param {string | VueNode} config.content
+ * @param {number} [config.duration] 自动关闭的延时，单位秒。设为 0 时不自动关闭。
+ * @param {string} [config.type] 'info' | 'success' | 'error' | 'warning' | 'loading'
+ * @param {string} [config.prefixCls]
+ * @param {string} [config.rootPrefixCls]
+ * @param {Function} [config.getPopupContainer]
+ * @param {Function} [config.onClose]
+ * @param {Function | VueNode} [config.icon]
+ * @param {string | number} [config.key]
+ * @param {CSSProperties} [config.style]
+ * @param {string} [config.class]
+ * @param {any} [config.appContext]
+ * @param {Function} [config.onClick]
+ * @returns 返回一个函数，可以调用该函数关闭弹窗
  */
 const messageNotice = (config: MessageArgsProps) => {
-  message.open(config);
+  return message.open(config);
 };
 
 export default messageNotice;
