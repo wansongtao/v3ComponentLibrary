61d2ab0a9901dec87bf09719314ddad54fc60445
wansongtao <18101837209@163.com>
2022-06-28 11:40:37 +0800

    perf:axios实例的request方法类型声明优化

--- src/api/common.ts
+++ src/api/common.ts
@@ -2,13 +2,13 @@ import instance from '@/utils/request';
 import { IBaseResponse } from './types/interface';
 
 export const test = <T = any>(): Promise<IBaseResponse<T>> => {
-  return instance.get('');
+  return instance.get('/');
 };
 
 export const login = <T = any>() => {
-  return instance.request<IBaseResponse<T>>({
-    url: '',
-    method: 'post'
+  return instance.request<IBaseResponse<T>, IBaseResponse<T>>({
+    url: '/test',
+    method: 'get'
   });
 };
 
