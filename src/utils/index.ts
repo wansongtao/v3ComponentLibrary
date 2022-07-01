10e754eae64753fc20a992e3612da7c98e2c1c37
wansongtao <18101837209@163.com>
2022-07-01 09:28:57 +0800

    perf:函数式编程实现优化

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -154,8 +154,12 @@ export const formatTime = (
  * @returns
  */
 export const compose = (...funcs: Function[]) => {
-  if (funcs.length <= 1) {
+  if (funcs.length === 0) {
     return (arg: any) => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
   }
 
   return funcs.reduce((a, b) => {
