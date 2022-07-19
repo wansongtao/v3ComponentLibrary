f89788cb37607f818d48a6c1a96426370c7c3e31
wansongtao <18101837209@163.com>
2022-07-19 11:11:10 +0800

    feat:添加分页器组件

--- components.d.ts
+++ components.d.ts
@@ -6,11 +6,13 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
+    APagination: typeof import('ant-design-vue/es')['Pagination']
     FileSelector: typeof import('./src/components/FileSelector/index.vue')['default']
     MySwipe: typeof import('./src/components/MySwipe/index.vue')['default']
     MySwipeItem: typeof import('./src/components/MySwipeItem/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SelfPagination: typeof import('./src/components/SelfPagination/index.vue')['default']
   }
 }
 
---
+++ src/components/SelfPagination/index.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+import { debounce } from '@/utils/index';
+
+const emit = defineEmits<{
+  (event: 'changepag', data: { page: number; size: number }): void;
+}>();
+const props = defineProps({
+  total: {
+    type: Number,
+    default: 0
+  },
+  currPage: {
+    type: Number,
+    default: 1
+  },
+  size: {
+    type: Number,
+    default: 10
+  },
+  delay: { // 防抖时间，单位毫秒，设置为0则不防抖
+    type: Number,
+    default: 300
+  }
+});
+
+const current = ref<number>(props.currPage);
+const pageSize = ref<number>(props.size);
+const onChange = debounce((page: number, size: number) => {
+  emit('changepag', { page, size });
+}, props.delay);
+
+watch([() => props.currPage, () => props.size], ([page, size]) => {
+  current.value = page;
+  pageSize.value = size;
+});
+</script>
+
+<template>
+  <div class="self_pagination">
+    <a-pagination
+      v-model:current="current"
+      v-model:pageSize="pageSize"
+      show-quick-jumper
+      :total="total"
+      :show-total="(total) => `共${total}条`"
+      :pageSizeOptions="['10', '20', '30', '40']"
+      :showSizeChanger="true"
+      @change="onChange"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.self_pagination {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  box-sizing: border-box;
+  padding: 0 20px;
+  width: 100%;
+  height: 50px;
+
+  .ant-pagination {
+    position: relative;
+
+    :deep(.ant-pagination-total-text) {
+      position: absolute;
+      top: 0;
+      left: -175px;
+    }
+
+    :deep(.ant-select) {
+      position: absolute;
+      top: 0;
+      left: -104px;
+    }
+  }
+}
+</style>
